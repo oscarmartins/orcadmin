404b6ce2be060176703eef06c8805726e68074be
Oscar Martins <oscarrafaelcampos@gmail.com>
2017-09-11 17:47:35 +0100

    vue input listners

---
+++ client/src/components/Register.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input 
+    type="text"
+    name="email"
+    v-model="email"
+    placeholder="email" />
+    <br>           
+    <input 
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password" />
+    <br>
+    <button>Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
