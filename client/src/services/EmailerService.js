271de21d7402494483c06fda5f7bacebea5bc004
Oscar Martins <oscarrafaelcampos@gmail.com>
2017-10-09 17:33:45 +0100

    fix

--- client/src/services/EmailerService.js
+++ client/src/services/EmailerService.js
@@ -1,8 +1,8 @@
 import Api from '@/services/Api'
 const path = 'emailer'
 export default {
-  fetchProfiles () {
-    const result = Api().get(path).then((result) => result)
+  async fetchProfiles () {
+    const result = await Api().get(path).then((result) => result)
     debugger
     result.data.fetchProfiles[0].pass = ''
     return result
