f6b067402d222b63ddfeddf88dcc447a67f8b0cd
orcmartins <orcmartins@localhost.localdomain>
2017-11-05 12:13:14 +0000

    gitignore

--- orccontext.js
+++ orccontext.js
@@ -6,13 +6,13 @@ module.exports = {
       /**connection: 'postgres://hostname/database_name'**/
       client: 'pg',
       connection: {
-        "host": "",
-        "database": "",
-        "user": "",
-        "password": ""
+        "host": "orcseven.com",
+        "database": "my_life",
+        "user": "oscar",
+        "password": "238229700"
       },
       useNullAsDefault: true
     }      
   },
-  jwtSecret: ''
+  jwtSecret: 'orcsecret'
 }
